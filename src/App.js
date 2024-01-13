73ffb57d0cea27be51a3be99cd7f248ee39097cc
giano182~ <amer.elghali2006@gmail.com>
2024-01-13 12:00:30 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './card';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <BasicExample/>
+    </div>
+  );
+}
+
+export default App;
